2d9c6025549ffdac67bcccab802148af8e9ec60e
HermitSun <syl1887415157@126.com>
2021-10-02 16:47:45 +0800

    wip: add network conf

---
+++ init/init.sql
@@ -0,0 +1,11 @@
+create database opencart;
+create database provdb;
+use provdb;
+create table users
+(
+    id       int         not null auto_increment,
+    username varchar(20) not null,
+    password varchar(20) not null,
+    primary key (id),
+    unique key (username)
+);
