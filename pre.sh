d9b09261ea3ac717a516bdc3f156b8fd5ee917f6
HermitSun <syl1887415157@126.com>
2021-10-06 14:04:29 +0800

    fix: fix incorrect scripts & env

--- pre.sh
+++ pre.sh
@@ -1,5 +1,4 @@
-# TODO: if upload_bak/ exists, copy & use it
-if [[ -f "upload_bak/" ]]; then
+if [[ -d "../upload_bak/" ]]; then
   cp -r ../upload_bak/ ../upload/
 else
   cp -r ../upload/ ../upload_bak/
@@ -8,3 +7,6 @@ sudo chmod 777 -R ../upload/
 mkdir mysql-logs
 sudo chmod 777 mysql-logs
 sudo docker-compose up -d
+# 这个确实没办法，现在的实现有问题
+# FIXME: 后面用别的方法去启动ssh
+sleep 10 && docker exec opencart-docker_provdb_1 /bin/sh -c 'service ssh start'
