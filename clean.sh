88c03d24c771c5b5d2fb577b62dedd6b67f878c1
HermitSun <syl1887415157@126.com>
2021-10-06 13:47:40 +0800

    refactor: update scripts

--- clean.sh
+++ clean.sh
@@ -1,6 +1,4 @@
 sudo docker-compose down
 sudo docker volume rm opencart-docker_mysql_data
 sudo rm -rf ./*-logs/
-
-# change workdir
-sudo rm -rf upload/
+sudo rm -rf ../upload/
--- pre.sh
+++ pre.sh
@@ -1,7 +1,10 @@
 # TODO: if upload_bak/ exists, copy & use it
-cp -r upload/ upload_bak/
-sudo chmod 777 -R upload/
-
-# change workdir
-mkdir mysql-logs && sudo chmod 777 mysql-logs
+if [[ -f "upload_bak/" ]]; then
+  cp -r ../upload_bak/ ../upload/
+else
+  cp -r ../upload/ ../upload_bak/
+fi
+sudo chmod 777 -R ../upload/
+mkdir mysql-logs
+sudo chmod 777 mysql-logs
 sudo docker-compose up -d
